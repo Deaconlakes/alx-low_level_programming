bfb4dd6034c723351c5151e96201b2dd5e4f29a2
Deaconlakes <alabiolalekan6@gmail.com>
2023-06-18 12:21:57 -0700

    program that print a -z excluding e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - funtion that print a-z excluding q && e
+ *
+ * Return: always 0
+ *
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		if (i != 'q' && i != 'e')
+			putchar(i);
+	putchar('\n');
+	return (0);
+}
