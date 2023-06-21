a67cb891e6df556cd65d3d1789b27e297e9d693b
Deaconlakes <alabiolalekan6@gmail.com>
2023-06-21 09:32:59 -0700

    Write a function that prints the 9 times table, starting with 0

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,9 +9,9 @@ void times_table(void)
 {
 	int a, b, product;
 
-	for (a = 0 ; a < 10 ; a++)
+	for (a = 0 ; a <= 8 ; a++)
 	{
-		for (b = 0 ; b < 10 ; b++)
+		for (b = 0 ; b < 9 ; b++)
 		{
 			product = a * b;
 			if (b == 0)
