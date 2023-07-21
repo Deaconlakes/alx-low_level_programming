cbbd503b1b761126714a91dff8c9341a2e14f336
Deaconlakes <alabiolalekan6@gmail.com>
2023-07-21 05:49:44 -0700

    define a new type struct dog with the element used in the doh_h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H
+#define _DOG_H
+/**
+ * struct dog - struct that create new type
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ * Desc: defining a new type of struct dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
