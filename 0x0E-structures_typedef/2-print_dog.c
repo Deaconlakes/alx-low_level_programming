6010328f06cef7081d8106ce8bdf837d767242ea
Deaconlakes <alabiolalekan6@gmail.com>
2023-07-21 06:51:23 -0700

    update fuunction that prints struct dog decleration

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,9 +1,9 @@
 #include "dog.h"
 #include <stdio.h>
 /**
- * print_dog - function that prints a struct dog decleration
- * @d: the new dog of the type struct
+ * print_dog - print a struct dog decleration
  *
+ * @d: the new dog of the type struct
  */
 void print_dog(struct dog *d)
 {
@@ -13,12 +13,11 @@ void print_dog(struct dog *d)
 			printf("Name: (nil)\n");
 		else
 			printf("Name: %s\n", d->name);
-
 		printf("Age: %f\n", d->age);
 
 		if (!(d->owner))
-			printf("Name: (nil)\n");
+			printf("Owner: (nil)\n");
 		else
-			printf("Name: %s\n", d->owner);
+			printf("Owner: %s\n", d->owner);
 	}
 }
