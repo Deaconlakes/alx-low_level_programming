f6e12c1fc849f8f7f4cce75f25e3b287348b21a7
Deaconlakes <alabiolalekan6@gmail.com>
2023-08-31 17:31:35 -0700

    task 1

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,18 +9,18 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-        int i;
-        unsigned int nums = 0;
+	int i;
+	unsigned int nums = 0;
 
-        if (!b)
-                return (0);
+	if (!b)
+		return (0);
 
-        for (i = 0; b[i]; i++)
-        {
-                if (b[i] < '0' || b[i] > '1')
-                        return (0);
-                nums = 2 * nums + (b[i] - '0');
-        }
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		nums = 2 * nums + (b[i] - '0');
+	}
 
-        return (nums);
+	return (nums);
 }
